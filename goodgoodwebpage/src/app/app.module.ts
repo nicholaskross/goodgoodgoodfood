e91458c7f6d60794975a0e815ce4f663627e418a
Boots <cnd9351@rit.edu>
2019-02-16 16:19:55 -0500

    search and filter created

--- goodgoodwebpage/src/app/app.module.ts
+++ goodgoodwebpage/src/app/app.module.ts
@@ -3,18 +3,23 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import {MatToolbarModule} from "@angular/material";
+import {MatExpansionModule, MatToolbarModule} from "@angular/material";
 import { NavbarComponent } from './components/navbar/navbar.component';
+import { SearchFilterComponent } from './components/search-filter/search-filter.component';
+import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent
+    NavbarComponent,
+    SearchFilterComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    MatToolbarModule
+    MatToolbarModule,
+    MatExpansionModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
