261e3ddbe4e3590f5bdc2e81957a58fd32083bd8
dmelcer9 <dmelcer9@gmail.com>
2019-02-16 17:09:06 -0500

    Began wegmans api

--- backend/productcatalog.py
+++ backend/productcatalog.py
@@ -3,6 +3,16 @@ from backend.product import Product
 
 
 class ProductCatalog:
+
+    def 
+
+    def ensure_search(self, query:str):
+
+
+    def search_query(self, query:str):
+        query = query.lower().strip()
+        self.ensure_search(query)
+
     def __init__(self):
         self.search_queries: Dict[str, List[int]] = {}  #: What search queries have been performed so far
         self.skus: Dict[int, Product]
