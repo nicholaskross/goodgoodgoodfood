82f3e60efbadf7afff56e62dc7a96164d90580ef
dmelcer9 <dmelcer9@gmail.com>
2019-02-17 12:52:23 -0500

    Don't crash when no description

--- backend/product.py
+++ backend/product.py
@@ -6,11 +6,11 @@ class Product(Dictable):
 
     def __init__(self, sku: int, image_url: str, brand: str, description: str, price: float, size: ValueUnit,
                  name: str):
-        self.name = name
+        self.name = name if name else ""
         self.size = size
         self.price = price
-        self.description = description
-        self.brand = brand
+        self.description = description if description else ""
+        self.brand = brand if brand else ""
         self.image_url = image_url
         self.sku = sku
         self.unit_price = ValueUnit(self.price / self.size.value, self.size.unit) \
