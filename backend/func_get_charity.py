d2cfe9e537b5f6ec09b49500f119d321d9c3afaa
nicholaskross <45185629+nicholaskross@users.noreply.github.com>
2019-02-17 10:05:51 -0500

    added more things to search for

--- backend/func_get_charity.py
+++ backend/func_get_charity.py
@@ -8,7 +8,7 @@ from backend.product import Product
 from backend.productcatalog import ProductCatalog
 
 categories = {
-    "haschildren": ["jr", "jr.", "kid", "kids", "cereal", "fruit snacks", "fun size", "family"],
+    "haschildren": ["jr", "jr.", "kid", "kids", "cereal", "snack", "fun size", "family"],
     "eco": ["organic", "environmental"],
     "lotsofseafood": ["fish", "salmon", "tuna", "tilapia", "shrimp", "lobster", "crab", "cod", "fillet", "swordfish"],
     "vegan": ["vegetarian", "vegan", "dairy-free", "soy", "chick'n", "chik'n", ]
